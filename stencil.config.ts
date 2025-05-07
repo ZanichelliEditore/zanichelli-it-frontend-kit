18c4b603bdb362648c2bac7d8e78e63aca166ffd
Dario <dariosacco9@gmail.com>
2025-05-07 15:53:54 +0200

    setup and styles

---
+++ stencil.config.ts
@@ -0,0 +1,24 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'zanichelli-it-frontend-kit',
+  globalStyle: 'src/style.css',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
